47612e082f0606b6397f119d33adaf5dc68400d2
Sumukha <sumukhamohan6@gmail.com>
2020-12-24 19:37:07 +0530

    other(swipe-end): removed swiping options at tab ends

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,46 +3,20 @@ import {
   OnInit,
   Output,
   EventEmitter,
-  HostListener,
 } from "@angular/core";
-import { trigger, keyframes, animate, transition } from "@angular/animations";
-import * as kf from "../../keyFrames";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
-  animations: [
-    trigger("swipeEndAnimation", [
-      transition("* => headShake", animate(1500, keyframes(kf.headShake))),
-    ]),
-  ],
 })
 export class HomeComponent implements OnInit {
   @Output() sendIndex: EventEmitter<any> = new EventEmitter<any>();
-  animationState: string;
   constructor() {}
 
   ngOnInit(): void {}
 
   handleSwipeLeft(): any {
     this.sendIndex.emit(1);
-  }
-
-  startAnimation(state): any {
-    console.log(state);
-    if (this.animationState !== state) {
-      this.animationState = state;
-    }
-  }
-
-  resetAnimationState(): any {
-    console.log("animation Ended");
-    this.animationState = "";
-  }
-
-  @HostListener("window:focus", ["$event"])
-  onFocus(event: any): void {
-    this.resetAnimationState();
   }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,11 @@
-import { EventEmitter, HostListener } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 import { Output } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
-import { trigger, keyframes, animate, transition } from "@angular/animations";
-import * as kf from "../../keyFrames";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
-  animations: [
-    trigger("swipeEndAnimation", [
-      transition("* => headShake", animate(1000, keyframes(kf.headShake))),
-    ]),
-  ],
 })
 export class MenuComponent implements OnInit {
   @Output() sendIndex: EventEmitter<any> = new EventEmitter<any>();
@@ -23,20 +16,5 @@ export class MenuComponent implements OnInit {
 
   handleSwipeRight(): any {
     this.sendIndex.emit(1);
-  }
-
-  startAnimation(state): any {
-    console.log(state);
-    if (this.animationState !== state) {
-      this.animationState = state;
-    }
-  }
-
-  resetAnimationState(): any {
-    this.animationState = "";
-  }
-  @HostListener("window:focus", ["$event"])
-  onFocus(event: any): void {
-    this.resetAnimationState();
   }
 }
